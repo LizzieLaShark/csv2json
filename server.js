4ddfaf5d38109f8025bfee0a00046b0b65d8ce53
lizzielashark <lizzie.s.sullivan@gmail.com>
2016-11-21 13:55:00 +0700

    used CLI to parse sample data

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
+
+
+//Converter Class 
 var Converter = require("csvtojson").Converter;
+var converter = new Converter({});
+converter.fromFile("./testDataCSVtoJSON.csv",function(err,result){
 
-//record_parsed will be emitted each csv row being processed
-converter.on("record_parsed", function (jsonObj) {
-   console.log(jsonObj); // <== our result json object
 });
-
-require("request").get
-("https://docs.google.com/spreadsheets/d/1yKWj5_TQkWSVlYvCbvxDxkpMYrHD-TuTUWbl9YcNorg/edit?usp=sharing")
-.pipe(converter);
-
-converter();
-
-
-/////***papaParse***////
-
-Papa.parse(csvString[, config])
